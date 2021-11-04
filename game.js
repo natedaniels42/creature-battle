fe2c6d06456b2524b31183154d2d4a37f3b01a4a
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-04 15:05:45 -0500

    Getting ready to pull

---
+++ game.js
@@ -0,0 +1,10 @@
+const Combat = require('./combatloop');
+const creatureCreate = require('./creatureCreate');
+
+// const playGame = async () => {
+//     const { creature1, creature2 } = await creatureCreate();
+//     console.log(creature1);
+//     //Combat(creature1, creature2);
+// }
+
+// playGame();
