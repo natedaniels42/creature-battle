2b33af5bd70b02c75119e426031f90e530c065b6
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-04 16:21:58 -0500

    Fixed bugs in creatureCreate

--- game.js
+++ game.js
@@ -5,15 +5,16 @@ const { matchupDescription } = require('./matchupDescription');
 
 const playGame = async () => {
     const { creature1, creature2 } = await creatureCreate();
-    //await matchupDescription(creature1, creature2);
+    await matchupDescription(creature1, creature2);
     prompt('Press enter to start fight');
-    while (creature1.lifepoints > 0 && creature2.lifepoints > 0) {
-        const alive = await Combat(creature1, creature2);
-        if (alive) {
-            await Combat(creature2, creature1);
-        }
-        await prompt('Press enter to continue');
-    }
+    // while (creature1.lifepoints > 0 && creature2.lifepoints > 0) {
+    //     let alive1 = await Combat(creature1, creature2);
+    //     let alive2;
+    //     if (alive1) {
+    //         alive2 = await Combat(creature2, creature1);
+    //     }
+    //     await prompt('Press enter to continue');
+    // }
 
 }
 
--- matchupDescription.js
+++ matchupDescription.js
@@ -1,8 +1,8 @@
 const matchupDescription = (c1, c2) => {
-    console.log(`Creature 1 is ${creature1.name} wielding the ${creature1.weapon.name}.\n
-    It does ${creature1.weapon.attack} damage and ${creature1.name} has ${creature1.lifepoints}.`);
-    console.log(`Creature 1 is ${creature2.name} wielding the ${creature2.weapon.name}.\n
-    It does ${creature2.weapon.attack} damage and ${creature2.name} has ${creature2.lifepoints}.`);
+    console.log(`Creature 1 is ${c1.name} wielding the ${c1.weapon.name}.
+${c1.weapon.name} does a max of ${c1.weapon.attack} damage and ${c1.name} has ${c1.lifepoints} health.`);
+    console.log(`\nCreature 1 is ${c2.name} wielding the ${c2.weapon.name}.
+${c2.weapon.name} does a max of ${c2.weapon.attack} damage and ${c2.name} has ${c2.lifepoints} health.`);
 }
 
 module.exports = {
