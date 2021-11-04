e665e9c9153f6b3f38ab583ef763f2c6584eb12d
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-04 15:29:08 -0500

    Pulling

--- game.js
+++ game.js
@@ -1,10 +1,17 @@
-const Combat = require('./combatloop');
-const creatureCreate = require('./creatureCreate');
+const prompt = require('prompt-sync')();
+const { Combat } = require('./combatloop');
+const { creatureCreate } = require('./creatureCreate');
 
-// const playGame = async () => {
-//     const { creature1, creature2 } = await creatureCreate();
-//     console.log(creature1);
-//     //Combat(creature1, creature2);
-// }
+const playGame = async () => {
+    const { creature1, creature2 } = await creatureCreate();
+    
+    while (creature1.lifepoints > 0 && creature2.lifepoints > 0) {
+        const alive = await Combat(creature1, creature2);
+        if (alive) {
+            await Combat(creature2, creature1);
+        }
+    }
 
-// playGame();
+}
+
+playGame();
