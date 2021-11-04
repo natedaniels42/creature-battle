b7183d8846fe696b8dc934d6d269529486d4e451
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-04 16:23:24 -0500

    Uncommented main game functionality

--- game.js
+++ game.js
@@ -7,14 +7,14 @@ const playGame = async () => {
     const { creature1, creature2 } = await creatureCreate();
     await matchupDescription(creature1, creature2);
     prompt('Press enter to start fight');
-    // while (creature1.lifepoints > 0 && creature2.lifepoints > 0) {
-    //     let alive1 = await Combat(creature1, creature2);
-    //     let alive2;
-    //     if (alive1) {
-    //         alive2 = await Combat(creature2, creature1);
-    //     }
-    //     await prompt('Press enter to continue');
-    // }
+    while (creature1.lifepoints > 0 && creature2.lifepoints > 0) {
+        let alive1 = await Combat(creature1, creature2);
+        let alive2;
+        if (alive1) {
+            alive2 = await Combat(creature2, creature1);
+        }
+        await prompt('Press enter to continue');
+    }
 
 }
 
